b51b5892f7187b5622a1a482a83305c87d5c85d7
Luke Boswell <lukewilliamboswell@gmail.com>
2026-08-31 10:36:23 +1000

    Pin keyed selection changed-set work

--- examples/_fixtures/js-framework-benchmark/specs/select_1k.scm
+++ examples/_fixtures/js-framework-benchmark/specs/select_1k.scm
@@ -6,5 +6,16 @@
     (click (role link :name "Select row 3"))
     (expect-attr (test-id "row-2") class "")
     (expect-attr (test-id "row-3") class "danger")
+    ; The selector dirties the old and new memberships only. The four Roc calls
+    ; are the two model projections and the two downstream class renderers;
+    ; none of the other 998 row renderers runs.
     (expect-metric-delta selector_members_dirtied 2)
-    (expect-metric-delta derived_calls_into_roc 4)))
+    (expect-metric-delta derived_calls_into_roc 4)
+    (expect-metric-delta patches_emitted 2)
+    (expect-metric-delta each_syncs 0)
+    (expect-metric-delta rows_created 0)
+    (expect-metric-delta rows_reused 0)
+    (expect-metric-delta rows_removed 0)
+    (expect-metric-delta active_graph_records_rebuilt 0)
+    (expect-metric-delta signal_record_table_rebuilt 0)
+    (expect-metric-delta stream_nodes_scanned 0)))
